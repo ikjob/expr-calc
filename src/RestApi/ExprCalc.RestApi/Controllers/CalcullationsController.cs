b0cd2c5bddc2d25f3d5a5f988ad79eb7cae3c603
ikjob <198514252+ikjob@users.noreply.github.com>
2025-02-15 04:36:49 +0300

    [4] Initialize Rest Api project

---
+++ src/RestApi/ExprCalc.RestApi/Controllers/CalcullationsController.cs
@@ -0,0 +1,36 @@
+﻿using ExprCalc.RestApi.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExprCalc.RestApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/calculations")]
+    public class CalcullationsController(
+        ILogger<CalcullationsController> logger) : ControllerBase
+    {
+        private readonly ILogger<CalcullationsController> _logger = logger;
+
+
+        [HttpGet]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Calculations list")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Type = typeof(ProblemDetails), Description = "Server error")]
+        public Task<ActionResult<IEnumerable<CalculationDto>>> GetCalculationsListAsync(CancellationToken token)
+        {
+            _logger.LogDebug("Get calculations list called");
+            return Task.FromResult<ActionResult<IEnumerable<CalculationDto>>>(
+                new CalculationDto[] {
+                    new CalculationDto { Id = 1 },
+                    new CalculationDto { Id = 2 },
+                    new CalculationDto { Id = 3 }
+                });
+        }
+    }
+}
