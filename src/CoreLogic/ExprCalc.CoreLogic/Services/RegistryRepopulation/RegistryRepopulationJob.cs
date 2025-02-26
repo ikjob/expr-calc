cbbb25e8521a26441044eb5afdd9bd55e969d44a
ikjob <198514252+ikjob@users.noreply.github.com>
2025-02-26 03:29:15 +0300

    [19] Implement calculation status reset in storage

---
+++ src/CoreLogic/ExprCalc.CoreLogic/Services/RegistryRepopulation/RegistryRepopulationJob.cs
@@ -0,0 +1,60 @@
+﻿using ExprCalc.CoreLogic.Configuration;
+using ExprCalc.CoreLogic.Instrumentation;
+using ExprCalc.CoreLogic.Resources.CalculationsRegistry;
+using ExprCalc.Storage.Api.Repositories;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExprCalc.CoreLogic.Services.RegistryRepopulation
+{
+    /// <summary>
+    /// Job that runs at the startup and repopulates <see cref="IScheduledCalculationsRegistry"/> from storage
+    /// </summary>
+    internal class RegistryRepopulationJob : BackgroundService
+    {
+        private readonly IScheduledCalculationsRegistry _calculationsRegistry;
+        private readonly ICalculationRepository _calculationsRepository;
+
+        private readonly DateTime _startTime;
+        private readonly int _singleBatchSize;
+
+        private readonly ActivitySource _activitySource;
+        private readonly ILogger<RegistryRepopulationJob> _logger;
+
+        public RegistryRepopulationJob(
+            IScheduledCalculationsRegistry calculationsRegistry,
+            ICalculationRepository calculationRepository,
+            IOptions<CoreLogicConfig> config,
+            ILogger<RegistryRepopulationJob> logger,
+            InstrumentationContainer instrumentation)
+        {
+            _startTime = DateTime.UtcNow;
+
+            _calculationsRegistry = calculationsRegistry;
+            _calculationsRepository = calculationRepository;
+
+            _singleBatchSize = Math.Max(10, config.Value.MaxRegisteredCalculationsCount / 2);
+
+            _activitySource = instrumentation.ActivitySource;
+            _logger = logger;
+        }
+
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Registry repopulation started");
+            await Task.Yield();
+
+
+
+            _logger.LogInformation("Registry repopulation finished");
+        }
+    }
+}
