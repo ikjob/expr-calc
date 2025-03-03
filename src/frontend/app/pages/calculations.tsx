711a27fd558fd4c0f0c8b826693c68fdb5d8de79
ikjob <198514252+ikjob@users.noreply.github.com>
2025-03-04 00:32:42 +0300

    [47] Expression submit component

--- src/frontend/app/pages/calculations.tsx
+++ src/frontend/app/pages/calculations.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { useCancelCalculationMutation, useGetCalculationsQuery } from '../api/calculations'
+import { useEffect, useRef, useState } from 'react'
+import { useCancelCalculationMutation, useCreateCalculationMutation, useGetCalculationsQuery } from '../api/calculations'
 import CalculationsTable from '../components/calculationsTable'
 import Pagination from '../components/pagination'
 import { User as UserIcon } from '@ricons/fa'
@@ -17,10 +17,16 @@ export default function CalculationsPage() {
     const [filters, setFilters] = useState({} as CalculationFilters);
     const [pagination, setPagination] = useState({ pageNumber: 1, pageSize: PAGE_SIZE } as PaginationParams);
     const { data: calculationRows, isLoading: calcLoading } = useGetCalculationsQuery({ filters: filters, pagination: pagination }, { pollingInterval: POLLING_INTERVAL_MS, skipPollingIfUnfocused: true });
+    const [createCalculationApi] = useCreateCalculationMutation();
     const [cancelCalculationApi] = useCancelCalculationMutation();
     //const { data: calculationsDelta } = useGetCalculationUpdatesQuery({ fromTime:  filters: filters })
 
 
+    function createCalculation(expression: string) {
+        if (activeUser) {
+            createCalculationApi({ expression: expression, createdBy: activeUser });
+        }
+    }
     function cancelCalculation(id: Uuid) {
         if (activeUser) {
             cancelCalculationApi({ id : id, cancelledBy: activeUser });
@@ -34,10 +40,7 @@ export default function CalculationsPage() {
 
     return (
         <div className="mt-2 mx-4">
-            <div className="join flex my-4">
-                <input type="text" className="input input-bordered input-accent join-item flex-auto" placeholder="Expression" />
-                <button className="btn btn-accent join-item rounded-r-full flex-none">Submit</button>
-            </div>
+            <CalculationSubmit onSubmitExpr={createCalculation} />
 
             <CalculationsFilter filters={filters} onChange={(newFilters) => setFilters(newFilters)} />
           
@@ -58,6 +61,49 @@ export default function CalculationsPage() {
         </div>
     )
 }
+
+
+interface CalculationSubmitProps {
+    onSubmitExpr: (expression: string) => void;
+}
+
+function CalculationSubmit(props: CalculationSubmitProps) {
+    const currentInput = useRef("");
+    const [inputErrorStatus, setInputErrorStatus] = useState({error: "", counter: 0});
+
+    useEffect(() => {
+        if (inputErrorStatus.error) {
+          const timeout = setTimeout(() => setInputErrorStatus({error: "", counter: inputErrorStatus.counter }), 5000);
+          return () => {
+            clearTimeout(timeout);
+          };
+        }
+      }, [inputErrorStatus]);
+
+
+    function submitExpression() {
+        if (currentInput.current != null && currentInput.current != "" && currentInput.current.trim() != "") {
+            props.onSubmitExpr(currentInput.current);
+            setInputErrorStatus({ error: "", counter: inputErrorStatus.counter });
+        } 
+        else {
+            setInputErrorStatus({ error: "Expression cannot be empty", counter: inputErrorStatus.counter + 1 });
+        }
+    }
+
+    return (
+        <div className="mt-4 mb-0">
+            <div className="join flex">
+                <input type="text" className="input input-bordered input-accent join-item flex-auto" placeholder="Expression" onChange={e => { currentInput.current = e.target.value; }} />
+                <button className="btn btn-accent join-item rounded-r-full flex-none" onClick={submitExpression}>Submit</button>
+            </div>
+            <span className="validator-hint text-error ml-1 mt-1 mb-0">
+                {inputErrorStatus.error}
+            </span>
+        </div>
+    )
+}
+
 
 
 interface CalculationsFilterProps {
