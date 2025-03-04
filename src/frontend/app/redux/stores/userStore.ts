eac496e853c60b6e2f54b863757db09b82e91ce1
ikjob <198514252+ikjob@users.noreply.github.com>
2025-03-04 04:16:09 +0300

    [47] Use localStorage to store user name

--- src/frontend/app/redux/stores/userStore.ts
+++ src/frontend/app/redux/stores/userStore.ts
@@ -2,12 +2,14 @@ import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 import type { RootState } from '../store'
 
+const USER_STORE_ACTIVE_USER_NAME = "exprCalcActiveUserName"
+
 interface UserState {
     userName: string | null
 }
 
 const initialState: UserState = {
-    userName: null,
+    userName: localStorage.getItem(USER_STORE_ACTIVE_USER_NAME),
 }
 
 export const activeUserStateSlice = createSlice({
@@ -15,7 +17,8 @@ export const activeUserStateSlice = createSlice({
     initialState,
     reducers: {
         setActiveUserName: (state, action: PayloadAction<string>) => {
-            state.userName = action.payload
+            state.userName = action.payload;
+            localStorage.setItem(USER_STORE_ACTIVE_USER_NAME, action.payload);
         },
     },
 })
