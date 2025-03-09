7de9357853b2d5c1cb36f4e78209a658c8e400b1
ikjob <198514252+ikjob@users.noreply.github.com>
2025-03-09 18:48:09 +0300

    [61] Combine repopulation and cleanup services to prevent interference between them

--- src/backend/CoreLogic/ExprCalc.CoreLogic/Services/StorageManagement/StorageCleanupJob.cs
+++ src/backend/CoreLogic/ExprCalc.CoreLogic/Services/StorageManagement/StorageCleanupJob.cs
@@ -1,9 +1,8 @@
 ﻿using ExprCalc.CoreLogic.Configuration;
 using ExprCalc.CoreLogic.Instrumentation;
 using ExprCalc.CoreLogic.Resources.CalculationsRegistry;
-using ExprCalc.CoreLogic.Services.RegistryRepopulation;
+using ExprCalc.CoreLogic.Services.StorageCleanup;
 using ExprCalc.Storage.Api.Repositories;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
@@ -13,58 +12,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExprCalc.CoreLogic.Services.StorageCleanup
+namespace ExprCalc.CoreLogic.Services.StorageManagement
 {
     /// <summary>
-    /// Periodically removes old calculations from storage
+    /// Removes old calculations from storage
     /// </summary>
-    internal class StorageCleanupService : BackgroundService
+    internal class StorageCleanupJob
     {
-        private readonly ICalculationRepository _calculationsRepository;
         private readonly IScheduledCalculationsRegistry _calculationRegistry;
+        private readonly ICalculationRepository _calculationsRepository;
 
         private readonly TimeSpan _expirationPeriod;
-        private readonly TimeSpan _cleanupPeriod;
 
         private readonly ActivitySource _activitySource;
-        private readonly ILogger<StorageCleanupService> _logger;
+        private readonly ILogger _logger;
 
-        public StorageCleanupService(
-            ICalculationRepository calculationRepository,
+        public StorageCleanupJob(
             IScheduledCalculationsRegistry calculationRegistry,
+            ICalculationRepository calculationRepository,
             IOptions<CoreLogicConfig> config,
-            ILogger<StorageCleanupService> logger,
+            ILogger logger,
             InstrumentationContainer instrumentation)
         {
-            _calculationsRepository = calculationRepository;
             _calculationRegistry = calculationRegistry;
+            _calculationsRepository = calculationRepository;
 
             _expirationPeriod = config.Value.StorageCleanupExpiration;
-            _cleanupPeriod = config.Value.StorageCleanupPeriod;
 
             _activitySource = instrumentation.ActivitySource;
             _logger = logger;
         }
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            if (_cleanupPeriod <= TimeSpan.Zero)
-            {
-                _logger.LogInformation("Storage cleanup was disabled");
-                return;
-            }
-
-            // Delay first run slightly
-            await Task.Delay(TimeSpan.FromSeconds(15), stoppingToken);
-
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                await RunCleanup(stoppingToken);
-                await Task.Delay(_cleanupPeriod, stoppingToken);
-            }
-        }
-
-        private async Task RunCleanup(CancellationToken token)
+        public async Task ExecuteAsync(CancellationToken token)
         {
             DateTime deleteBefore = DateTime.UtcNow.Subtract(_expirationPeriod);
             _logger.LogInformation("Cleanup procedure started");
