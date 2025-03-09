353d96f97f15818c9ac1b5b013fabbf234ca768b
ikjob <198514252+ikjob@users.noreply.github.com>
2025-03-10 00:11:23 +0300

    [48] Submitted date range filters (not finished)

--- src/frontend/app/components/calculationsFilter.tsx
+++ src/frontend/app/components/calculationsFilter.tsx
@@ -51,7 +51,6 @@ function UserFilter({ filters, onChange }: CalculationsFilterProps) {
             <label className="input w-70 mr-3">
                 <input type="text" className="" placeholder="User name" maxLength={32} disabled={isActiveUserCheck} 
                     ref={userInputRef}
-                    value={isActiveUserCheck ? (filters.createdBy ?? "") : undefined} 
                     defaultValue={filters.createdBy ?? ""}
                     onKeyDown={(e) => { if (e.key == "Enter") { onUserNameChanged((e.target as HTMLInputElement).value); } } }
                     onBlur={(e) => onUserNameChanged((e.target as HTMLInputElement).value) } />
@@ -74,7 +73,9 @@ const UserFilterContextBound = connect((state: RootState) => { return { activeUs
 
 export function CalculationsFilter({ filters, onChange }: CalculationsFilterProps) {
     const expressionInputRef = useRef<HTMLInputElement>(null);
-    const anyFilter = filters.createdBy || filters.state || filters.expression || false;
+    const dateFromInputRef = useRef<HTMLInputElement>(null);
+    const dateToInputRef = useRef<HTMLInputElement>(null);
+    const anyFilter = filters.createdBy || filters.state || filters.expression || filters.createdAtMin || filters.createdAtMax || false;
 
     function onSelectStateChange(newState: string) {
         if (newState != filters.state) {
@@ -101,6 +102,31 @@ export function CalculationsFilter({ filters, onChange }: CalculationsFilterProp
         }
     }
 
+    function onDateFromChanged(newDateFrom: string) {
+        const parsedDate = newDateFrom ? Date.parse(newDateFrom) : 0;
+        if (parsedDate != filters.createdAtMin) {
+            const newFilters = { ...filters };
+            newFilters.createdAtMin = parsedDate ? parsedDate : undefined;
+            onChange(newFilters);
+            
+            if (dateFromInputRef.current != null) {
+                dateFromInputRef.current.value = newDateFrom;
+            }
+        }
+    }
+    function onDateToChanged(newDateTo: string) {
+        const parsedDate = newDateTo ? Date.parse(newDateTo) : 0;
+        if (parsedDate != filters.createdAtMax) {
+            const newFilters = { ...filters };
+            newFilters.createdAtMax = parsedDate ? parsedDate : undefined;
+            onChange(newFilters);
+            
+            if (dateToInputRef.current != null) {
+                dateToInputRef.current.value = newDateTo;
+            }
+        }
+    }
+
     return (
         <details className="collapse collapse-arrow bg-base-200 border-base-300 rounded-md my-4">
             <summary className="collapse-title">
@@ -108,12 +134,14 @@ export function CalculationsFilter({ filters, onChange }: CalculationsFilterProp
                 { !anyFilter ? <div className="badge badge-lg mx-4">All</div> : <></>}
                 { filters.expression ? <div className="badge badge-lg mx-4 max-w-120"><span className="text-info">Expression:</span><span className="truncate">{filters.expression.substring(0, Math.min(120, filters.expression.length))}</span></div> : <></>}
                 { filters.createdBy ? <div className="badge badge-lg mx-4"><span className="text-info">Submitted by:</span> <UserIcon className="w-4 h-[1em] inline relative text-base-content" /> {filters.createdBy}</div> : <></>}
+                { filters.createdAtMin ? <div className="badge badge-lg mx-4"><span className="text-info">Submitted after:</span>{new Date(filters.createdAtMin).toLocaleString()}</div> : <></>}
+                { filters.createdAtMax ? <div className="badge badge-lg mx-4"><span className="text-info">Submitted before:</span>{new Date(filters.createdAtMax).toLocaleString()}</div> : <></>}
                 { filters.state ? <div className="badge badge-lg mx-4"><span className="text-info">State:</span> {filters.state}</div> : <></>}
             </summary>
             <div className="collapse-content mt-2">
                 <div className="mt-2">
-                    <span className="mr-1 align-middle">Expression: </span>
-                    <label className="input w-100 mr-3">
+                    <span className="mr-5 align-middle">Expression: </span>
+                    <label className="input w-150 mr-3">
                         <input type="text" className="" placeholder="Expression substring" maxLength={25000}
                             ref={expressionInputRef}
                             defaultValue={filters.expression ?? ""}
@@ -125,11 +153,35 @@ export function CalculationsFilter({ filters, onChange }: CalculationsFilterProp
                     </label>
                 </div>
                 <div className="mt-2">
-                    <span className="mr-12 align-middle">User: </span>
+                    <span className="mr-16 align-middle">User: </span>
                     <UserFilterContextBound filters={filters} onChange={onChange} />
                 </div>
                 <div className="mt-2">
-                    <span className="mr-11 align-middle">State: </span>
+                    <span className="mr-1 align-middle">Submitted at: </span>
+                    <label className="input w-70">
+                        <input type="datetime-local" className=""
+                            ref={dateFromInputRef}
+                            defaultValue={filters.createdAtMin ? new Date(filters.createdAtMin).toLocaleString() : undefined}
+                            onKeyDown={(e) => { if (e.key == "Enter") { onDateFromChanged((e.target as HTMLInputElement).value); } } }
+                            onBlur={(e) => onDateFromChanged((e.target as HTMLInputElement).value) } />
+                        <button onClick={() => onDateFromChanged("")}>
+                            <TimesIcon className="w-3 h-3"  />
+                        </button>
+                    </label>
+                    <span className="mx-3 align-middle">to</span>
+                    <label className="input w-70 mr-3">
+                        <input type="datetime-local" className=""
+                            ref={dateToInputRef}
+                            defaultValue={filters.createdAtMin ? new Date(filters.createdAtMin).toLocaleString() : undefined}
+                            onKeyDown={(e) => { if (e.key == "Enter") { onDateToChanged((e.target as HTMLInputElement).value); } } }
+                            onBlur={(e) => onDateToChanged((e.target as HTMLInputElement).value) } />
+                        <button onClick={() => onDateToChanged("")}>
+                            <TimesIcon className="w-3 h-3"  />
+                        </button>
+                    </label>
+                </div>
+                <div className="mt-2">
+                    <span className="mr-15 align-middle">State: </span>
                     <select defaultValue="Pick a status" className="select select-sm w-70" onChange={(e) => onSelectStateChange(e.target.value)}>
                         <option value="">---</option>
                         <option value="Pending">Pending</option>
